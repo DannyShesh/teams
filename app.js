983f02cce99525b2f85b7e2bee527be9253141e0
DannyShesh <hannes.skola1@gmail.com>
2022-02-11 12:36:21 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const main = document.getElementById('main')
 const send = document.getElementById('knapp')
 const box = document.getElementById('chat-box')
+const chat = document.getElementById('chat')
+
+let users = []
 
 send.addEventListener('click',function(e){
     e.preventDefault()
     const text = document.getElementById('text')
-    console.log(text.value);
+    let showText = document.createElement('li')
+    showText.innerHTML = text.value + "    " + users
+    chat.appendChild(showText)
+    console.log(users);
+    
     
 })
+
+
 
 main.addEventListener('submit',function(e){
     e.preventDefault()
@@ -17,6 +26,7 @@ let uname = document.getElementById('uname')
 box.hidden = false
 main.hidden = true
 
+users.push(uname.value)
 console.log(uname.value);
 console.log(psw.value);
 })
